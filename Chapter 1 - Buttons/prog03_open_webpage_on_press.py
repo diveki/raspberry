80b2a412979b37e58058ed9a8b08418efbd0c718
Zsolt <divekizs@yahoo.com>
2020-05-03 22:57:11 +0200

    button press opens webpage is done

--- Chapter 1 - Buttons/prog03_open_webpage_on_press.py
+++ Chapter 1 - Buttons/prog03_open_webpage_on_press.py
@@ -1,3 +1,11 @@
 from gpiozero import Button
 from signal import pause
+import os
 
+button = Button(2)
+
+browser = ['google-chrome', 'firefox', 'chromium-browser']
+
+while True:
+    button.wait_for_press()
+    os.system(f'{browser[0]} www.facebook.com')  # this assumes you have google-chrome installed
