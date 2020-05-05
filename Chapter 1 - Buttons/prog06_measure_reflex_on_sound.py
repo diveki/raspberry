1de7565ed3b27aff5eca6a6d89330fee5692671d
zsolt.pi <zsolt.pi@pi.org>
2020-05-05 21:28:39 +0000

    reflex game added to chapter 1

--- Chapter 1 - Buttons/prog06_measure_reflex_on_sound.py
+++ Chapter 1 - Buttons/prog06_measure_reflex_on_sound.py
@@ -1,3 +1,31 @@
 from gpiozero import Button
-from signal import pause
+import os
+import time
+import random
 
+button = Button(2)
+
+N = 5    # number of trials
+waiting_times = list(range(1,11))
+delays = []
+
+sounds = os.listdir('./sounds')
+
+for n in range(N):
+	print(f'Game {n+1} started')
+	time.sleep(1)
+	print('Press the button when signal heard')
+	ran = random.choice(waiting_times)
+	time.sleep(ran)
+#	os.system('aplay sounds/{0}'.format(sounds[ran-1]))
+	print('press now')
+
+	start = time.time()
+	button.wait_for_press()
+	end = time.time()
+	delay = end - start
+	delays.append(delay)
+
+print(f'The game is over')
+reaction = sum(delays) / N
+print(f'Your average reaction took {reaction:.2f} seconds')
