98fbcd627aa919e7b27df6035095dbcc57ede328
Zsolt <divekizs@yahoo.com>
2020-05-07 23:24:13 +0200

    first version of button encoder done in chapter 1

--- Chapter 1 - Buttons/prog07_morse_signals_with_press_length.py
+++ Chapter 1 - Buttons/prog07_morse_signals_with_press_length.py
@@ -6,6 +6,7 @@ button = Button(2)
 press = 0
 release = 0
 
+# open file for the secret message
 msg = open('message.txt','w')
 
 code_map = {'trambulin':'lsls',
@@ -24,22 +25,26 @@ def end():
 	release = time.time()
     delay = release - press
     code = encoder(delay, msg)
+    if code == 'close':
+        msg.close()
+        print('Encripted message saved')
+        print('Please exit the program!')
+    if not msg.closed():
+        msg.write(code)
 
-def encoder(delay, msg):
+def encoder(delay):
     if delay >= 1 and delay < 4:
         return 'l'
     elif delay < 1:
         return 's'
     elif delay >= 4 and delay < 7:
-        msg.write('\n')
+        return '\n'
     else:
-        msg.close()
-        print('Encripted message saved')
+        return 'close'
 
 
 button.when_pressed = start
 button.when_released = end
 
 while True:
-    button.wait_for_press()
-    button.wait_for_release()
+    pass
