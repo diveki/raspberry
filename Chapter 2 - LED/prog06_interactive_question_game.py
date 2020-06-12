6181156b30da62cdc2379c8d02c1f891b666e4dc
Divéki Zsolt PhD <divekizs@eli-alps.hu>
2020-06-12 16:27:31 +0200

    led codes are continued

--- Chapter 2 - LED/prog06_interactive_question_game.py
+++ Chapter 2 - LED/prog06_interactive_question_game.py
@@ -1,5 +1,59 @@
-from gpiozero import RGBLED, Button
+from gpiozero import RGBLED, Button, LED
 from time import sleep
+import random
 
+blue = LED(9)
+red = LED(10)
 
-led = RGBLED(red=9, green=10, blue=11)
+yes = Button(2)
+no  = Button(3)
+
+qa = {
+    'Törökország': 'Ankara',
+    'Albánia'    : 'Tirana',
+    'Argentína'  : 'Buenos Aires',
+    'Ausztria'   : 'Bécs',
+    'Görögország': 'Athén',
+    'Szerbia'    : 'Belgrád',
+    'Németország': 'Berlin',
+    'Ausztrália' : 'Canberra',
+    'Írország'   : 'Dublin',
+    'Norvégia'   : 'Oslo',
+}
+
+press = False
+right_answer = 0
+
+def question(country):
+    txt = f'Mi {country} fővárosa?'
+    return txt
+
+def options(city, opt):
+    global right_answer
+    a = [random.choice(opt)]
+    a.append(city)
+    random.shuffle(a)
+    txt = f'1) {a[0]}      2) {a[1]}'
+    right_answer = a.index(city) + 1
+    return txt
+
+def pressed(button, pos = 0):
+    global press, right_answer
+    if pos == right_answer:
+        print('You got it right!')
+    else:
+        print('Wroooong!')
+    press = False
+
+yes.when_pressed = pressed(yes, pos = 1)
+no.when_pressed  = pressed(no, pos = 2)
+
+while True:
+    if not press:
+        press = True
+        country = random.choice(list(qa.keys()))
+        city    = qa[country]
+        print(question(country))
+        print('='*10)
+        print(options(city, list(qa.values())))
+        print('='*10)
