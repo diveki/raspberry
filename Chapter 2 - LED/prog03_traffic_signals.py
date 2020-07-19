d942323d7f38704349a7acce7b2cfb86702bf28f
zsolt.pi <zsolt.pi@pi.org>
2020-07-19 08:02:24 +0000

    right pins added to prog03 in led

--- Chapter 2 - LED/prog03_traffic_signals.py
+++ Chapter 2 - LED/prog03_traffic_signals.py
@@ -1,33 +1,33 @@
 from gpiozero import LED
 from time import sleep
 
-red = LED(2)
-amber = LED(3)
-green = LED(4)
+#red = LED(22)
+#amber = LED(27)
+#green = LED(17)
 
-green.on()
-amber.off()
-red.off()
+#green.on()
+#amber.off()
+#red.off()
 
-while True:
-    sleep(10)
-    green.off()
-    amber.on()
-    sleep(1)
-    amber.off()
-    red.on()
-    sleep(10)
-    amber.on()
-    sleep(1)
-    green.on()
-    amber.off()
-    red.off()
+#while True:
+#    sleep(10)
+#    green.off()
+#    amber.on()
+#    sleep(1)
+#    amber.off()
+#    red.on()
+#    sleep(10)
+#    amber.on()
+#    sleep(1)
+#    green.on()
+#    amber.off()
+#    red.off()
 
 
 # from gpiozero import TrafficLights
 # from time import sleep
 
-# lights = TrafficLights(2, 3, 4)
+# lights = TrafficLights(22, 27, 17)
 
 # lights.green.on()
 
@@ -46,23 +46,23 @@ while True:
 #     lights.red.off()
 
 
-# from gpiozero import TrafficLights
-# from time import sleep
-# from signal import pause
+from gpiozero import TrafficLights
+from time import sleep
+from signal import pause
 
-# lights = TrafficLights(2, 3, 4)
+lights = TrafficLights(22, 27, 17)
 
-# def traffic_light_sequence():
-#     while True:
-#         yield (0, 0, 1) # green
-#         sleep(10)
-#         yield (0, 1, 0) # amber
-#         sleep(1)
-#         yield (1, 0, 0) # red
-#         sleep(10)
-#         yield (1, 1, 0) # red+amber
-#         sleep(1)
+def traffic_light_sequence():
+    while True:
+        yield (0, 0, 1) # green
+        sleep(10)
+        yield (0, 1, 0) # amber
+        sleep(1)
+        yield (1, 0, 0) # red
+        sleep(10)
+        yield (1, 1, 0) # red+amber
+        sleep(1)
 
-# lights.source = traffic_light_sequence()
+lights.source = traffic_light_sequence()
 
-# pause()
+pause()
