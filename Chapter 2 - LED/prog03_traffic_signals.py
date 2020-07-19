f1f2dace0f533fead78f8734c32408d698f731df
Zsolt <divekizs@yahoo.com>
2020-07-19 15:49:09 +0200

    prog03 from led description finished

--- Chapter 2 - LED/prog03_traffic_signals.py
+++ Chapter 2 - LED/prog03_traffic_signals.py
@@ -1,27 +1,27 @@
 from gpiozero import LED
 from time import sleep
 
-#red = LED(22)
-#amber = LED(27)
-#green = LED(17)
+red = LED(22)
+amber = LED(27)
+green = LED(17)
 
-#green.on()
-#amber.off()
-#red.off()
+green.on()
+amber.off()
+red.off()
 
-#while True:
-#    sleep(10)
-#    green.off()
-#    amber.on()
-#    sleep(1)
-#    amber.off()
-#    red.on()
-#    sleep(10)
-#    amber.on()
-#    sleep(1)
-#    green.on()
-#    amber.off()
-#    red.off()
+while True:
+   sleep(10)
+   green.off()
+   amber.on()
+   sleep(1)
+   amber.off()
+   red.on()
+   sleep(10)
+   amber.on()
+   sleep(1)
+   green.on()
+   amber.off()
+   red.off()
 
 
 # from gpiozero import TrafficLights
@@ -46,23 +46,23 @@ from time import sleep
 #     lights.red.off()
 
 
-from gpiozero import TrafficLights
-from time import sleep
-from signal import pause
-
-lights = TrafficLights(22, 27, 17)
-
-def traffic_light_sequence():
-    while True:
-        yield (0, 0, 1) # green
-        sleep(10)
-        yield (0, 1, 0) # amber
-        sleep(1)
-        yield (1, 0, 0) # red
-        sleep(10)
-        yield (1, 1, 0) # red+amber
-        sleep(1)
-
-lights.source = traffic_light_sequence()
-
-pause()
+# from gpiozero import TrafficLights
+# from time import sleep
+# from signal import pause
+#
+# lights = TrafficLights(22, 27, 17)
+#
+# def traffic_light_sequence():
+#     while True:
+#         yield (0, 0, 1) # green
+#         sleep(10)
+#         yield (0, 1, 0) # amber
+#         sleep(1)
+#         yield (1, 0, 0) # red
+#         sleep(10)
+#         yield (1, 1, 0) # red+amber
+#         sleep(1)
+#
+# lights.source = traffic_light_sequence()
+#
+# pause()
