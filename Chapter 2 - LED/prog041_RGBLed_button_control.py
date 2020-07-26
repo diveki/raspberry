64091be391c9469cbabf63d40b7884175187f3dd
Zsolt <divekizs@yahoo.com>
2020-07-26 12:52:14 +0200

    prog04 description from led finished

--- Chapter 2 - LED/prog041_RGBLed_button_control.py
+++ Chapter 2 - LED/prog041_RGBLed_button_control.py
@@ -10,13 +10,13 @@ button3 = Button(17)
 
 
 def red():
-    led.color(1,0,0)
+    led.color = (1,0,0)
 
 def magenta():
-    led.color(1,0,1)
+    led.color = (1,0,1)
 
 def cyan():
-    led.color(0,1,1)
+    led.color = (0,1,1)
 
 button1.when_pressed = red
 button2.when_pressed = magenta
