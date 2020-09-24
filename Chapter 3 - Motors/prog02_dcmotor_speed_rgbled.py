f739a5ffebf988f0b34fe9a9f7685b431dd4dfe8
Zsolt <divekizs@yahoo.com>
2020-09-24 23:06:39 +0200

    prog02 from motors is finished

--- Chapter 3 - Motors/prog02_dcmotor_speed_rgbled.py
+++ Chapter 3 - Motors/prog02_dcmotor_speed_rgbled.py
@@ -18,19 +18,29 @@ def start_stop(button, m):
         on_off = True
         return m.forward
 
-def speed(increment = 0.1):
+def speed(button, increment = 0.1):
     global led, motor
     new_speed = motor.value + increment
     if new_speed > 0 and new_speed < 1.01:
         motor.value = new_speed
+        color = value2rgb(0,1,new_speed)
+        led.color = color
     else:
         print('Motor speed is out of range of 0 and 1.')
         print(f'Current speed is: {motor.value}')
 
+def value2rgb(minimum, maximum, value):
+    minimum, maximum = float(minimum), float(maximum)
+    ratio = 2 * (value-minimum) / (maximum - minimum)
+    b = int(max(0, 255*(1 - ratio)))/255
+    r = int(max(0, 255*(ratio - 1)))/255
+    g = (255 - b - r)/255
+    return r, g, b
+
 
 button_start_stop.when_pressed = start_stop(button_start_stop, motor)
-button_faster.when_pressed = speed(increment=0.1)
-button_slower.when_pressed = speed(increment=-0.1)
+button_faster.when_pressed = speed(button_faster, increment=0.1)
+button_slower.when_pressed = speed(button_slower, increment=-0.1)
 
 while True:
     pass
