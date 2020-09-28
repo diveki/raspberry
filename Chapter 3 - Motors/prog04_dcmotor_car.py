cda60dd610e937d701de9b3e62a12cc6c164f3e4
Divéki Zsolt PhD <divekizs@eli-alps.hu>
2020-09-28 19:16:41 +0200

    lucky wheel for motor is done

---
+++ Chapter 3 - Motors/prog04_dcmotor_car.py
@@ -0,0 +1,23 @@
+from gpiozero import Motor, Button
+import random
+import time
+
+motor = Motor(forward=23, backward = 24, enable=25)  # Motor(23,24,25)
+button_forward = Button(17)
+button_backward = Button(17)
+button_left = Button(17)
+button_right = Button(17)
+
+def start(button, m):
+    t_rot = random.uniform(0, 5)
+    speed = random.uniform(0.6, 1)
+    m.forward(speed=speed)
+    time.sleep(t_rot)
+    m.stop()
+
+
+button_start.when_pressed = start(button_start, motor)
+
+
+while True:
+    pass
