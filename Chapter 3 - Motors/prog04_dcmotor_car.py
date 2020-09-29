c62b54d0097af2f6e5757f3e0645f971a1a87ae7
Divéki Zsolt PhD <divekizs@eli-alps.hu>
2020-09-29 19:50:22 +0200

    motor car program started

--- Chapter 3 - Motors/prog04_dcmotor_car.py
+++ Chapter 3 - Motors/prog04_dcmotor_car.py
@@ -2,21 +2,25 @@ from gpiozero import Motor, Button
 import random
 import time
 
-motor = Motor(forward=23, backward = 24, enable=25)  # Motor(23,24,25)
-button_forward = Button(17)
-button_backward = Button(17)
-button_left = Button(17)
-button_right = Button(17)
+motor_left = Motor(forward=7, backward = 8, enable=25)  #
+motor_right = Motor(forward=9, backward = 10, enable=11)  # 
 
-def start(button, m):
-    t_rot = random.uniform(0, 5)
-    speed = random.uniform(0.6, 1)
-    m.forward(speed=speed)
-    time.sleep(t_rot)
-    m.stop()
+button_forward = Button(14)
+button_backward = Button(15)
+button_left = Button(18)
+button_right = Button(23)
 
+def move_forward(button, m1, m2):
+    pass
 
-button_start.when_pressed = start(button_start, motor)
+def move_backward(button, m1, m2):
+    pass
+
+def move_left(button, m1, m2):
+    pass
+
+def move_right(button, m1, m2):
+    pass
 
 
 while True:
