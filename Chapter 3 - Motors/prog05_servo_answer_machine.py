48645c8c19c5e86b2e7b154302e5dc2341be226f
Divéki Zsolt PhD <divekizs@eli-alps.hu>
2020-10-05 13:45:45 +0200

    servo answer machine program started

---
+++ Chapter 3 - Motors/prog05_servo_answer_machine.py
@@ -0,0 +1,14 @@
+from gpiozero import Servo, Button
+import random
+import time
+
+question_words = ['hogy', 'hogyan', 'miként', 'hol', 'kinél', 'kitől', 'hol', 'honnan', 
+                    'honnét', 'hová', 'hova', 'Meddig', 'Merre', 'Mettől', 'Minél',
+                    'Mitől', 'Ki', 'Kié', 'Kiért', 'Kiig', 'Kihez', 'Kiként', 'Kinek',
+                    'Kitől', 'Kivel', 'Mennyi', 'Hány', 'Mi', 'Mié', 'Miért', 'Miig',
+                    'Mihez', 'Miként', 'Minek', 'Mitől', 'Mivel', 'Mikor', 'Hánykor',
+                    'Hányig', 'Hánytól', 'Meddig', 'Mettől', 'Mikor', 'Mikortól', 
+                    'Milyen']
+
+while True:
+    pass
