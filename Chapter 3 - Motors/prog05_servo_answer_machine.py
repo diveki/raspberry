794b931714b683a37f5c0fc8b66fc7b170d68059
Divéki Zsolt PhD <divekizs@eli-alps.hu>
2020-10-05 17:53:45 +0200

    servo answer machine program continued

--- Chapter 3 - Motors/prog05_servo_answer_machine.py
+++ Chapter 3 - Motors/prog05_servo_answer_machine.py
@@ -1,4 +1,5 @@
 from gpiozero import Servo, Button
+import unidecode  # pip or conda install might be needed
 import random
 import time
 
@@ -9,6 +10,21 @@ question_words = ['hogy', 'hogyan', 'miként', 'hol', 'kinél', 'kitől', 'hol',
                     'Mihez', 'Miként', 'Minek', 'Mitől', 'Mivel', 'Mikor', 'Hánykor',
                     'Hányig', 'Hánytól', 'Meddig', 'Mettől', 'Mikor', 'Mikortól', 
                     'Milyen']
+
+# check if the question contains any of the above words
+def check_for_words(txt, word_list = question_words):
+    txt = txt.lower()
+    txt = unidecode.unidecode(txt)
+    txt = txt.replace('?','')
+    txt_list = txt.split(' ')
+
+    for word in word_list:
+        wl = unidecode.unidecode(word.lower())
+        if wl in txt_list:
+            return True
+    return False
+
+
 
 while True:
     pass
