b9f6ca986cdbf0a43ff29ec700d6b9090dde40f6
Divéki Zsolt PhD <divekizs@eli-alps.hu>
2020-10-09 16:36:40 +0200

    webpage plan developed further

--- Business/Webpage plan/picur_mockup/webpage/urls.py
+++ Business/Webpage plan/picur_mockup/webpage/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('kurzusok/', views.kurzusok, name='kurzusok'),
+    path('kurzusok/kurzus1/', views.kurzus1, name='kurzus1'),
+    path('kurzusok/kurzus2/', views.kurzus2, name='kurzus2'),
+    path('kurzusok/kurzus3/', views.kurzus3, name='kurzus3'),
+    path('kurzusok/kurzus4/', views.kurzus4, name='kurzus4'),
+    path('iskolak/', views.iskolak, name='iskolak'),
 ]
--- Business/Webpage plan/picur_mockup/webpage/views.py
+++ Business/Webpage plan/picur_mockup/webpage/views.py
@@ -8,3 +8,63 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'main.html', context=context)
+
+def kurzusok(request):
+    """View function for home page of site."""
+
+    context = {}
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'kurzusok.html', context=context)
+
+def kurzus1(request):
+    content = 'To be determined ...'
+    context = {'title': 'Kurzus 1',
+                'introduction': 'Sok sok bla bla',
+                'age': '10- ...',
+                'duration': '1.5 h',
+                'equipments': 'Everything is provided',
+                'prerequisit': 'Semmi',
+                'content': content}
+    return render(request, 'kurzus_reszletes.html', context=context)
+
+def kurzus2(request):
+    content = 'To be determined ...'
+    context = {'title': 'Kurzus 2',
+                'introduction': 'Sok sok bla bla',
+                'age': '10-...',
+                'duration': '1.5 h',
+                'equipments': 'Everything is provided',
+                'prerequisit': 'Kurzus 1',
+                'content': content}
+    return render(request, 'kurzus_reszletes.html', context=context)
+
+def kurzus3(request):
+    content = 'To be determined ...'
+    context = {'title': 'Kurzus 3',
+                'introduction': 'Sok sok bla bla',
+                'age': '10-...',
+                'duration': '1.5 h',
+                'equipments': 'Everything is provided',
+                'prerequisit': 'Kurzus 2',
+                'content': content}
+    return render(request, 'kurzus_reszletes.html', context=context)
+
+def kurzus4(request):
+    content = 'To be determined ...'
+    context = {'title': 'Kurzus 4',
+                'introduction': 'Sok sok bla bla',
+                'age': '10-...',
+                'duration': '1.5 h',
+                'equipments': 'Everything is provided',
+                'prerequisit': 'Kurzus 3',
+                'content': content}
+    return render(request, 'kurzus_reszletes.html', context=context)
+
+def iskolak(request):
+    items = [{'city': 'Szeged', 'address': 'Eli-Alps, 6728 Szeged, Wolfgang Sandner utca 3', 'link': ''},
+              {'city': 'Budapest', 'address': '1211 Budapest, Petofi Sandor utca 12', 'link': ''},
+              {'city': 'Algyő', 'address': '6750 Algyő, Tiszavvirág utca 27', 'link': ''}
+              ]
+    context = {'items': items}
+    return render(request, 'iskolak.html', context = context)
