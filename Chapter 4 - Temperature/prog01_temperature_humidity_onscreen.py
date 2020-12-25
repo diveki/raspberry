1179ee0839b4706c437c405df82fb0ca07f22773
Your Name <you@example.com>
2020-12-25 23:36:04 +0000

    webpages to temperature added

---
+++ Chapter 4 - Temperature/prog01_temperature_humidity_onscreen.py
@@ -0,0 +1,4 @@
+https://thepihut.com/blogs/raspberry-pi-tutorials/18095732-sensors-temperature-with-the-1-wire-interface-and-the-ds18b20
+http://docs.37sensors.com/#
+https://sensorkit.en.joy-it.net/index.php?title=KY-028_Temperature_Sensor_module_(Thermistor)
+https://www.malnasuli.hu/leckek/homerseklet-merese-1-wire-szenzorral/
