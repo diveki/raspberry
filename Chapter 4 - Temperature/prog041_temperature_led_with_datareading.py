3c2fd6e99b609768ed6c4800c2b45bdd67f91298
zsolt.pi <zsolt.pi@pi.org>
2021-01-21 23:04:59 +0000

    prog041 done in chapter 4

--- Chapter 4 - Temperature/prog041_temperature_led_with_datareading.py
+++ Chapter 4 - Temperature/prog041_temperature_led_with_datareading.py
@@ -1,27 +1,56 @@
 from gpiozero import MCP3008, LED
+import numpy as np
+from scipy.interpolate import interp1d
+
 
 mcp = MCP3008(channel=7)
 red = LED(21)
 yellow = LED(20)
 green = LED(16)
 
-def lightning(volt, leds=[green, yellow, red]):
-	if volt > 1.8:
+def read_mapping(name):
+	f = open(name, 'r')
+	data = f.readlines()
+	f.close()
+	data.pop(0)
+	tc = []
+	r = []
+	for line in data:
+		clean_data = line.strip().split(',')
+		tc.append(float(clean_data[0]))
+		r.append(float(clean_data[1])*1000)
+	return np.array(tc), np.array(r)
+	
+def voltage2resistance(v_out, v_in=3.3, r=9.82e3):
+	return r * v_out / (v_in - v_out)
+
+def interpolate_temperature(resistance, tc, r):
+	f = interp1d(r,tc)
+	return f(resistance)
+	
+def lightning(temp, leds=[green, yellow, red]):
+	if temp < 23:
 		for led in leds:
 			led.off()
-	elif volt <= 1.8 and volt > 1.5:
+	elif temp < 28 and temp >= 23:
 		leds[0].on()
 		leds[1].off()
 		leds[2].off()
-	elif volt <= 1.5 and volt > 1.3:
+	elif temp < 32 and temp >= 28:
 		leds[0].on()
 		leds[1].on()
 		leds[2].off()
-	elif volt <= 1.3:
+	elif temp >= 32:
 		leds[0].on()
 		leds[1].on()
 		leds[2].on()
 
+
+tc, resistance = read_mapping('ntcc.csv')
+
 while True:
-	lightning(mcp.voltage, leds=[green, yellow, red])
+	rt = voltage2resistance(mcp.voltage, v_in=3.3, r=9.82e3)
+	t_therm = interpolate_temperature(rt, tc, resistance)
+	lightning(t_therm, leds=[green, yellow, red])
+	print(t_therm)
 
