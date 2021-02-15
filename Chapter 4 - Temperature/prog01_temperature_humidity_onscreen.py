bdfa598b0322c9db4fb4b2824ae1710029aaa7fd
Divéki Zsolt PhD <divekizs@eli-alps.hu>
2021-02-15 17:32:21 +0100

    description for prog01 in chapter 4 is almost finished

--- Chapter 4 - Temperature/prog01_temperature_humidity_onscreen.py
+++ Chapter 4 - Temperature/prog01_temperature_humidity_onscreen.py
@@ -15,17 +15,17 @@ base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
  
-def read_temp_raw():
-    f = open(device_file, 'r')
+def read_temp_raw(file_name):
+    f = open(file_name, 'r')
     lines = f.readlines()
     f.close()
     return lines
  
-def read_temp():
-    lines = read_temp_raw()
+def read_temp(file_name):
+    lines = read_temp_raw(file_name)
     while lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
-        lines = read_temp_raw()
+        lines = read_temp_raw(file_name)
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
@@ -34,5 +34,5 @@ def read_temp():
         return temp_c, temp_f
 	
 while True:
-	print(read_temp())	
+	print(read_temp(device_file))	
 	time.sleep(1)
