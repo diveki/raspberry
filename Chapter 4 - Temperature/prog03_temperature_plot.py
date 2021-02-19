8a9eff2e378beb2ce3498b1b512f82f76eb95c86
Divéki Zsolt PhD <divekizs@eli-alps.hu>
2021-02-19 23:45:13 +0100

    description of prog021 in chapter 4 done

--- Chapter 4 - Temperature/prog03_temperature_plot.py
+++ Chapter 4 - Temperature/prog03_temperature_plot.py
@@ -3,20 +3,24 @@ import datetime as dt
 
 filename = 'test.csv'
 
-def read_data(fn, delimiter=','):
-	f = open(fn, 'r')
-	lines = f.readlines()
-	f.close()
+def read_temp_raw(file_name):
+    f = open(file_name, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_data(lines, delimiter=','):
 	date = []
 	tc = []
 	for line in lines[1:]:
 		a = line.strip().split(',')
 		dd = dt.datetime.strptime(a[0], '%Y-%m-%d %H:%M:%S')
 		date.append(dd)
-		tc.append(a[1])
+		tc.append(float(a[1]))
 	return date, tc
 
-d,t = read_data(filename)
+lines = read_temp_raw(filename)
+d,t = read_data(lines, delimiter=',')
 
 plt.plot(d,t, 'o-')
 plt.gcf().autofmt_xdate()
