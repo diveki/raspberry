1bf5f563a28d915fa19c2eece63367008d94d20e
Pi105 <pi@example.com>
2021-10-26 00:21:00 +0200

    raspberry git cloning bash script added

---
+++ Intro/folder_preparation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir Picur
+echo "Picur folder created"
+cd Picur
+echo "Starting to clone raspberry git into Picur folder..."
+git status clone https://github.com/diveki/raspberry.git
