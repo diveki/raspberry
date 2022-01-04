e0ef6ccd759778e2f819f1ce2a180bc1990a6866
Your Name <you@example.com>
2022-01-04 11:34:44 +0100

    prog06 in chapter 6 tested and worked

--- Chapter 6 - IR/prog06_rgbled_distance.py
+++ Chapter 6 - IR/prog06_rgbled_distance.py
@@ -1,7 +1,7 @@
 import threading
 import time
 from raspberry_functions import read_2column_files, interpolate1d, ActiveSensor
-from gpiozero import LED, MCP3008
+from gpiozero import LED, MCP3008, RGBLED
 import datetime as dt
 import matplotlib.pyplot as plt
 import random
@@ -9,7 +9,7 @@ import random
 
 class RGBActiveSensor(ActiveSensor):
 	def __init__(self, rgb, led, mcp, calibname):
-		super.__init__(self, led, mcp, calibname)
+		super().__init__(led, mcp, calibname)
 		self.rgb = rgb
 
 	def start(self):
@@ -23,7 +23,8 @@ class RGBActiveSensor(ActiveSensor):
 	def start_rgb(self):
 		print('Rgb started')
 		while not self.event.is_set():
-			self.set_color(self.ylist[-1])
+			if len(self.ylist) > 0:
+				self.set_color(self.ylist[-1])
 			time.sleep(1)
 
 	def stop(self):
@@ -33,83 +34,21 @@ class RGBActiveSensor(ActiveSensor):
 		print('Measurement is stopped')
 
 	def set_color(self, value):
-		if value < 0.7:
+		if value < 10:
 			self.rgb.color = (0,1,0) # green
-		elif 0.7 <= value < 1.4:
+		elif 10 <= value < 17:
 			self.rgb.color = (1,1,0) # yellow
-		elif value >= 1.4:
+		elif value >= 17:
 			self.rgb.color = (1,0,0) # red
 
 
 
-
-# class ActiveSensorTest:
-# 	def __init__(self):
-# 		self.event = threading.Event()
-# 		self.event_plot = threading.Event()
-# 		self.dlist = []
-# 		self.ylist = []
-		
-# 	def start(self):
-# 		self.event.clear()
-# 		t = threading.Thread(target=self.start_measurement)
-# 		t.start()
-
-# 	def start_measurement(self):
-# 		print('Measurement started')
-# 		while not self.event.is_set():
-# 			dd = dt.datetime.now()
-# 			current_voltage = random.uniform(0,10)
-# 			print(f'Current distance from object is: {current_voltage:.2} cm')
-# 			self.prepare_data(dd, current_voltage)
-# 			time.sleep(1)
-		
-# 	def stop(self):
-# 		self.event.set()
-# 		print('Measurement is stopped')
-
-# 	def initialize_calibration(self, filename):
-# 		self.calib_volt, self.calib_distance = read_2column_files(filename, header=True)
-
-# 	def prepare_data(self, dd, yy, maxlen=20):
-# 		self.dlist.append(dd)
-# 		self.ylist.append(yy)
-# 		if len(self.dlist) > maxlen:
-# 			self.dlist.pop(0)
-# 			self.ylist.pop(0)
-
-# 	def plot_initialize(self):
-# 		plt.ion()
-# 		self.figure, self.ax = plt.subplots(figsize=(8,6))
-# 		self.line1, = self.ax.plot(self.dlist, self.ylist, 'o-')
-# 		plt.title("Dynamic Plot of temperature",fontsize=25)
-# 		plt.xlabel("Time",fontsize=18)
-# 		plt.ylabel("Temperature (C)",fontsize=18)
-# 		plt.grid(True)
-
-# 	def start_plot(self):
-# 		self.event_plot.clear()
-# 		t = threading.Thread(target=self.plot_distance_thread)
-# 		t.start()
-
-
-# 	def plot_distance_thread(self):
-# 		while not self.event_plot.is_set():
-# 			self.line1.set_xdata(self.dlist)
-# 			self.line1.set_ydata(self.ylist)
-# 			self.ax.set_ylim(min(self.ylist)*0.99, max(self.ylist)*1.01) # +1 to avoid singular transformation warning
-# 			self.ax.set_xlim(min(self.dlist), max(self.dlist))
-# 			self.figure.canvas.draw()
-# 			self.figure.canvas.flush_events()
-# 			plt.gcf().autofmt_xdate()
-# 			time.sleep(2)
-
 if __name__ == '__main__':
+	rgb = RGBLED(16,20,21)
 	ir = LED(2)
 	mcp = MCP3008(channel=7)
 	calib_file = 'ir_calibration.csv'
-	a = ActiveSensor(ir, mcp, calib_file)
-	# a = ActiveSensorTest()
+	a = RGBActiveSensor(rgb, ir, mcp, calib_file)
 
 
 # For development:
