4368994e4310848054d521f3ebc68d8d6d8f1afd
Your Name <you@example.com>
2022-03-16 22:49:44 +0100

    notes of prog05 continued in chapter 6

---
+++ Chapter 6 - IR/studentVersion/prog02_distance_calibration_studentVersion.py
@@ -0,0 +1,14 @@
+# importald be a LED es MCP3008 klasszokat
+# a raspberry_functions_studentVersion modulbol importald be a read_2column_files es az interpolate1d fuggvenyeket
+
+
+ir = # inicializalj egy LEDet a GPIO 2es pinre
+mcp = # inicializald az ADC konvertert a 7es csatornaval
+
+calib_file = # add meg a feszultseg tavolsag kalibralas fajl nevet sztring formaban (ha kell az utvonalat is)
+volt, distance = # a read_2column_files fuggvennyel olvasd ki a kalibracios ertekeket, figyelj a bemeneti parameterekre
+
+while True:
+    current_voltage = # olvasd ki az ADC konverteren mert feszultseget
+    current_distance = # az interpolate1d fuggvennyel interpolald a mert feszultseget, hogy megbecsuld a tavolsagot
+    # nyomtasd ki a tavolsagot a kepernyore valami kisero szoveggel
